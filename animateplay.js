11adf2fef4782d0ed7954c7e5bad849de6c9ba75
wrule <1982775886@qq.com>
2017-04-11 10:03:38 +0800

    add
    add

---
+++ animateplay.js
@@ -0,0 +1,10 @@
+﻿jQuery.fn.extend({
+    play: function (css3, fun) {
+        $(this).removeClass(css3)
+               .addClass(css3)
+               .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                   $(this).removeClass(css3);
+                   fun();
+               });
+    }
+});
